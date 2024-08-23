165194bb06657821f41522139d82011bc3239212
Tux G <48701217+edgonzalez24@users.noreply.github.com>
2024-08-23 10:25:23 -0600

    chore: Dockerize and setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // Add ConfigModule to use the .env file
+    ConfigModule.forRoot(),
+    // Setup the database connection
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, // Load all entities from the entities folder (.entity.ts files)
+      synchronize: true, // Synchronize the database schema with the entities (drops and creates tables)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // New instance of Logger to log custom messages to the console
+  const logger = new Logger('Boostrap');
+  // Add global middlewares E.g '{base-url}/api/{endpoint}'
+  app.setGlobalPrefix('api');
+  // Add global pipes E.g 'ValidationPipe' to validate request body and query params before controller method execution
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
+  logger.log(`App running on port: ${process.env.PORT}`);
 }
 bootstrap();
